7fefd75936ef173cf05b319ab6d3b3bafc036786
Eduardo Parada <102472995+eduardoparada@users.noreply.github.com>
2022-10-12 15:49:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//function Avisar(){
+//    alert ("Aviso: A Prova vai ser f....");
+//}
+//function Iniciar(){
+//
+//}
+//function Resultado(){
+//
+//}
+var jogador1; // variavel global
+var jogador2; // variavel global
+
+function Escolha1(){
+    jogador1="1";
+}
+function Escolha2(){
+    jogador1="2";
+}
+function Escolha3(){
+    jogador1="3";
+}
+function Escolha4(){
+    jogador2="4";}
+function Escolha5(){
+    jogador2="5";
+}
+function Escolha6(){
+    jogador2="6";
+}
